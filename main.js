03f8364b80eeffd2748d977d5343c10893ff96a1
Michelle Wong <michellekwong2@gmail.com>
2020-03-19 18:00:05 -0400

    For loop to check if one-to-one mapping exists between s1 and s2

--- main.js
+++ main.js
@@ -7,21 +7,28 @@
 //Remapping a letter means replacing all occurrences of it with another letter while the ordering of the letters remains unchanged.
 
 function charMapping(s1, s2){
-    //if the length of string 1 doesnt equal length of string 2, return false 
     //optimize by setting variable names to both strings length
     let s1Length = s1.length
     let s2Length = s2.length
-        
-    if(s1Length !== s2Length){
-        return false;
-    }    
      //set chars variable object to store chars from s1 as key & values from s2
     let setChars = {};
 
+    //if the length of string 1 doesnt equal length of string 2, return false 
+    if(s1Length !== s2Length){
+        return false;
+    }    
     //for loop that'll loop through each char in s1 
+    for (let i=0 ; i < s1Length ; i++){
         //see if the char exists in setChars
         //if the char doesnt exist as a key in setChars
-            //then add it where s2 char equals the string
-        //if the char exists as a key in setChars already then compare it with s2 char
-
+        if(!setChars[s1[i]]) {
+            //then add it & set s1 char to equal s2 char 
+            setChars[s1[i]] = s2[i];
+        }
+        //if the char exists as a key in setChars already and does not equal s2 char then return false
+        else if (setChars[s1[i]] !== s2[i]){
+            return false;
+        }
+    }
+    return true;
 }
