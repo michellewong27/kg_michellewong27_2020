078bf5f2298e56870288b9247f656194b525be47
Michelle Wong <michellekwong2@gmail.com>
2020-03-19 18:50:51 -0400

    Final version taking out some comments

--- main.js
+++ main.js
@@ -9,15 +9,13 @@ function charMapping(s1, s2){
     if(s1Length !== s2Length){
         return false;
     }    
-    //for loop that'll loop through each char in s1 
+    //loop through each char in s1 
     for (let i=0 ; i < s1Length ; i++){
-        //see if the char exists in setChars
-        //if the char doesnt exist as a key in setChars
+        //if the char doesnt exist as a key in setChars then add it & set s1 char to equal s2 char 
         if(!setChars[s1[i]]) {
-            //then add it & set s1 char to equal s2 char 
             setChars[s1[i]] = s2[i];
         }
-        //if the char exists as a key in setChars already and does not equal s2 char then return false
+        //if the char exists as a key in setChars already & does not equal s2 char then return false
         else if (setChars[s1[i]] !== s2[i]){
             return false;
         }
